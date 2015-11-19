8cb1f4f93f07b9edd25db2214d64615bc77c143d
Ethan McCosky <emccoskyaustin@gmail.com>
2015-11-19 14:13:41 -0600

    Tabs Created and TableModels started

---
+++ ScoutingApplication/src/scoutapp/RankingsTableModel.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package scoutapp;
+
+import javax.swing.table.AbstractTableModel;
+
+/**
+ *
+ * @author mccosky_890578
+ */
+public class RankingsTableModel extends AbstractTableModel{
+    
+    private String[] headings = {"Rank", "Team", "MMR", "Avg. Score", "Range", "Consistency", "MMR of Partners", "QP", "RP"};
+    private String[][]  data;
+    
+    public RankingsTableModel (String[][] tableData){
+       data = tableData; 
+    }
+    
+    @Override
+    public String getColumnName(int col) {
+        return headings[col];
+    }
+   
+    @Override
+    public int getRowCount() {
+        return data.length;
+    }
+
+    @Override
+    public int getColumnCount() {
+        return data[0].length;    
+    }
+    
+    //MOVE OUT OF HERE! 
+    /*public void addMatch(String[] matchData) {
+        for (int i = 0; i < getColumnCount(); i++){
+            data[getRowCount()][i] = matchData[i];
+        }
+    }*/
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+       return data[rowIndex][columnIndex];
+    }
+}
